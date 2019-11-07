37c89b766abb099d871c0f39a62fd5f19346e918
william-oliveira-da-silva <wyll.hello@gmail.com>
2019-11-07 17:58:27 -0200

    dia 07/11

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -5,6 +5,13 @@ import './inicio.css';
 import down from '../img/download.jpg';
 import down1 from '../img/download1.jpg';
 import down2 from '../img/download2.jpg';
+import img from '../img/image.png';
+import img1 from '../img/image1.png';
+import img2 from '../img/image2.png';
+import img5 from '../img/image5.png';
+import img6 from '../img/image6.png';
+import img7 from '../img/image7.png';
+import img8 from '../img/image8.png';
 import Menu from './Menu';
 import logo_medium from '../img/logo_medium.png';
 
@@ -59,6 +66,63 @@ class Inicio extends Component {
             </Carousel>
 
           </div>
+          <div className="equipamentos">
+            <h1>Equipamentos</h1>
+
+            <div className="row">
+
+              <div className="col-md-4">
+                <h4>Serra de Mesa</h4>
+                <img src={img} alt="imagem de uma serra de mesa"/>
+                <p>Indicado para cortes de grandes peças de madeiras</p>
+              </div>
+
+              <div className="col-md-4">
+                <h4>Impressora 3D</h4>
+                <img src={img1} alt="imagem de uma impressora 3d"/>
+                <p>Criação de protopipos fisicos tridimensionais</p>
+              </div>
+
+              <div className="col-md-4">
+                <h4>CNC Router</h4>
+                <img src={img2} alt="imagem de CNC router"/>
+                <p>Corte e gravações em madeiras, MDF, Acrilico, entre outros materiais</p>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="materiais">
+            <h1>Materiais</h1>
+            <div className="row">
+              <div className="col-md-3">
+                <h4>Filamento LPA</h4>
+                <img src={img5} alt="imagem de filamentos lpa"/>
+
+              </div>
+
+              <div className="col-md-3">
+              <h4>Folhas de EVA</h4>
+                <img src={img6} alt="imagem de folhas de EVA"/>
+
+              </div>
+
+              <div className="col-md-3">
+              <h4>Arduino</h4>
+                <img src={img7} alt="imagem de arduino"/>
+
+              </div>
+
+              <div className="col-md-3">
+              <h4>Chapas de MDF</h4>
+                <img src={img8} alt="imagem de folhas de mdf" />
+
+              </div>
+
+            </div>
+          </div>
+             
         </div>
       </BrowserRouter>
     );
