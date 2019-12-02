8be6d0c6c14fbe0b7bd0953a34b8e450ac2c8cdc
Renata Rodrigues <renata-rm@hotmail.com>
2019-12-02 17:24:52 -0300

    estilização de paginas Eventos, Início, Cantato

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import { BrowserRouter } from 'react-router-dom';
+import { Row, Col, Container } from 'react-bootstrap';
+
+
 import './inicio.css';
+import down from '../img/download.jpg';
+import down1 from '../img/download1.jpg';
+import down2 from '../img/download2.jpg';
 import img from '../img/image.png';
 import img1 from '../img/image1.png';
 import img2 from '../img/image2.png';
@@ -9,103 +16,161 @@ import img6 from '../img/image6.png';
 import img7 from '../img/image7.png';
 import img8 from '../img/image8.png';
 import Menu from './Menu';
-import Carrocel from './Carrocel';
-import logo from '../../src/img/logo.png';
-
+import logo from '../img/logo.png';
 
 class Inicio extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
-      <img className= "logoIF" src={logo} alt="Logo"/>       
-      </div>
-        <div className="conteiner">
-          <Menu />
-          <div className="imgInicio">
-            <Carrocel/>  
-             </div>
-          <div className="equipamentos">
-            <h1>Equipamentos</h1>
+        <div>
+          <div className="conteiner">
 
-            <div className="row">
-              <div className="col-md-4">
-                <h4>Serra de Mesa</h4>
-                <img src={img} alt="imagem de uma serra de mesa" />
-                <p>Indicado para cortes de grandes peças de madeiras</p>
-              </div>
+            {/* Logotipo do ifmaker */}
+            <img className="logoIF" src={logo} alt="Logo" />
+    
+            <Menu />
 
-              <div className="col-md-4">
-                <h4>Impressora 3D</h4>
-                <img src={img1} alt="imagem de uma impressora 3d" />
-                <p>Criação de prototipos físicos tridimensionais</p>
-              </div>
 
-              <div className="col-md-4">
-                <h4>CNC Router</h4>
-                <img src={img2} alt="imagem de CNC router" />
-                <p>Corte e gravações em madeiras, MDF, Acrilico, entre outros materiais</p>
-              </div>
+            <div className="imgCarrocel">
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={down}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={down1}
+                    alt="Third slide"
+                  />
 
-            </div>
+                  <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={down2}
+                    alt="Third slide"
+                  />
 
-          </div>
-
-          <div className="materiais">
-            <h1>Materiais</h1>
-            <div className="row">
-              <div className="col-md-3">
-                <h4>Filamento LPA</h4>
-                <img src={img5} alt="imagem de filamentos lpa" />
-              </div>
-
-              <div className="col-md-3">
-                <h4>Folhas de EVA</h4>
-                <img src={img6} alt="imagem de folhas de EVA" />
-              </div>
-
-              <div className="col-md-3">
-                <h4>Arduino</h4>
-                <img src={img7} alt="imagem de arduino" />
-              </div>
-
-              <div className="col-md-3">
-                <h4>Chapas de MDF</h4>
-                <img src={img8} alt="imagem de folhas de mdf" />
-
-              </div>
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
 
             </div>
-          </div>
-          <div className="sobre">
-            <h3>O que é</h3>
-            <h5>
-              IFMaker é uma plataforma de prototipagem com suporte à inovação e ao aprendizado.
+            <div className="equipamentos">
+              <Container>
+                <Row >
+                  <Col md={4}>
+                    <h4>Serra de Mesa</h4>
+                    <img src={img} alt="imagem de uma serra de mesa" />
+                    <p>Indicado para cortes de grandes peças de madeiras</p>
+                  </Col>
+                  <Col md={4}>
+                    <h4>Impressora 3D</h4>
+                    <img src={img1} alt="imagem de uma impressora 3d" />
+                    <p>Criação de protopipos fisicos tridimensionais</p>
+                  </Col>
+                  <Col md={4}>
+                    <h4>CNC Router</h4>
+                    <img src={img2} alt="imagem de CNC router" />
+                    <p>Corte e gravações em madeiras, MDF, Acrilico, entre outros materiais</p>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
 
-              Possibilita que pessoas da comunidade interna e externa ao IFMS campus aquidauana
-              possam projetar e desenvolver suas ideias de forma criativa e cooperativa.
+            <div className="materiais">
+              <h1>Materiais</h1>
+              
+              <Container>
+                <Row >
+                  <Col md={3}>
 
-              O objetivo principal é a disseminação do conhecimento técnico fornecendo estimulo
-              para a difusão do empreendedorismo local.
+                    <h4>Filamento LPA</h4>
+                    <img src={img5} alt="imagem de filamentos lpa" />
 
-              Se você possui uma ideia - ainda que julgue ser inconsistente - venha explorar o potencial da mesma,
-              colocando-a em prática de forma divertida e coloborativa.
-            </h5>
+                  </Col>
+                  <Col md={3}>
+                    <h4>Folhas de EVA</h4>
+                    <img src={img6} alt="imagem de folhas de EVA" />
 
-          </div>
-        </div>
+                  </Col>
+                  <Col md={3}>
 
+                    <h4>Arduino</h4>
+                    <img src={img7} alt="imagem de arduino" />
 
-        <div className="rodaPe">
-          <div className="local">
-            <p>2009-2019 Instituto Federal de Educação, Ciência e Tecnologia de
-              Mato Grosso do Sul. Todos os direitos reservados.
+                  </Col>
+                  <Col md={3}>
+                    <h4>Chapas de MDF</h4>
+                    <img src={img8} alt="imagem de folhas de mdf" />
+
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+            <Col md={12} className="texto">
+              <h3>O QUE É</h3>
+              <p>
+                IFMaker é uma plataforma de prototipagem com suporte à inovação e ao aprendizado.
+            </p>
+              <p>
+                Possibilita que pessoas da comunidade interna e externa ao IFMS campus aquidauana
+                possam projetar e desenvolver suas ideias de forma criativa e cooperativa.
+            </p>
+
+              <p>
+                O objetivo principal é a disseminação do conhecimento técnico fornecendo estimulo
+                para a difusão do empreendedorismo local.
+            </p>
+
+              <p>
+                Se você possui uma ideia - ainda que julgue ser inconsistente - venha explorar o potencial da mesma,
+                colocando-a em prática de forma divertida e coloborativa.
+            </p>
+              <h3>QUEM SOMOS</h3>
+              <p>
+                Somos uma equipe de professores, técnicos,
+                pesquisadores e estudantes que possuem conhecimento
+                técnico e científico em diversas áreas.
+            </p>
+
+              <p>
+                Estamos dispostos a ensinar, cooperar e aprender.
+            </p>
+              <p>
+                Sim, a aprender, pois a multidisciplinaridade é um dos fatores presentes no IFMaker.
+            </p>
+            </Col>
+
+            <div className="rodaPe">
+              <div className="local">
+                <p>2009-2019 Instituto Federal de Educação, Ciência e Tecnologia de
+                  Mato Grosso do Sul. Todos os direitos reservados.
               </p>
-            <p>Rua José Tadao Arima, 222- Vila Ycaraí - Aquidauana/ MS - CEP: 79200-000</p>
-            <p>Telefone:(67)3240-1600</p>
-          </div>
-          <div className="autores">
-            <span>2019 por Renata & William</span>
+                <p>Rua José Tadao Arima, 222- Vila Ycaraí - Aquidauana/ MS - CEP: 79200-000</p>
+
+              </div>
+              <div className="autores">
+                <span>2019 por Renata & William</span>
+              </div>
+            </div>
+
+
           </div>
         </div>
       </BrowserRouter>
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './menu.css';
+import './Inicio';
 
 class Menu extends Component {
     render() {
         return (
-            <div className="fundo">
-                <div className="container">                     
-                    <ul className="nav">
+             <div>  
+                    <div className="nav">
                         <li className="nav-item">
                             <a className="nav-link" href="Inicio">Home</a>
                         </li>
-                        
+
                         <li className="nav-item">
-                            <a className="nav-link" href="Sobre">Sobre Nós</a>
+                            <a className="nav-link" href="Eventos">Eventos</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="Contato">Contato</a>
@@ -22,12 +22,11 @@ class Menu extends Component {
                             <div className="dropdown-menu">
                                 <a className="dropdown-item" href="Cursos">Cursos</a>
                                 <a className="dropdown-item" href="Agendamento">Agendamento</a>
-                                <a className="dropdown-item" href="Eventos">Eventos</a></div>
+                            </div>
                         </li>
-                    </ul>
-                </div>
-            </div>
-
+                    </div>
+                </div> 
+             
         );
     }
 }
